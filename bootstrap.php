76f992cb341bed664edac43e225a2e1a15de5e0d
+f0r3v3r+ <forestscholar@gmail.com>
2016-09-18 17:19:59 -0400

    Update bootstrap.php
    Changed license in bootstrap.php to be more accurate.

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,15 @@
 <?php
 /*
- * This file is part of flagrow/flarum-ext-image-upload.
+ * This file is part of Push-EDX/flarum-ext-chat
+ * Commented lines in this file originally taken from part of flagrow/flarum-ext-image-upload.
++ *
++ * Copyright (c) Flagrow.
++ *
++ * http://flagrow.github.io
+ * 
+ * Copyright (c) under MIT License courtesy of Push-EDX.
  *
- * Copyright (c) Flagrow.
- *
- * http://flagrow.github.io
+ * http://pushedx.net/
  *
  * For the full copyright and license information, please view the license.md
  * file that was distributed with this source code.
@@ -15,7 +20,6 @@ namespace PushEDX\Chat;
 //use Flagrow\ImageUpload\Providers\StorageServiceProvider;
 use Flarum\Foundation\Application;
 use Illuminate\Contracts\Events\Dispatcher;
-
 
 return function (Dispatcher $events, Application $app) {
 
